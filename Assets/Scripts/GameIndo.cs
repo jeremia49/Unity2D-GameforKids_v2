f53c8e69d7a67f27f1241c4a192d4f3f38069479
Jeremia M <39850621+jeremia49@users.noreply.github.com>
2022-07-22 13:05:08 +0700

    add ipa + randomize indo

--- Assets/Scripts/GameIndo.cs
+++ Assets/Scripts/GameIndo.cs
@@ -5,10 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class GameIndo : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Source;
+    public Vector3[] originalPos;
+
     void Start()
     {
         
+    }
+
+    void Awake()
+    {
+        Vector3[] originalPos = new Vector3[Source.transform.childCount];
+
+        for (int i = 0; i < Source.transform.childCount; i++)
+        {
+            originalPos[i] = Source.transform.GetChild(i).position;
+        }
+
+        reshuffle(originalPos);
+
+        for (int i = 0; i < Source.transform.childCount; i++)
+        {
+            Source.transform.GetChild(i).position = originalPos[i];
+        }
+    }
+
+    void reshuffle(Vector3[] pos)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < pos.Length; t++)
+        {
+            Vector3 tmp = pos[t];
+            int r = Random.Range(t, pos.Length);
+            pos[t] = pos[r];
+            pos[r] = tmp;
+        }
     }
 
     // Update is called once per frame
